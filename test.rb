d8a50d0554ca3653d7227ce9a4204aa8d63b9a4c
derek pelt <derekpelt@gmail.com>
2018-04-05 03:50:23 -0400

    pinrts out links

--- test.rb
+++ test.rb
@@ -1,12 +1,20 @@
 require 'nokogiri' 
 require 'open-uri'
 
+@links = [] 
+
 doc = Nokogiri::HTML(open("https://www.myinstants.com/index/ca/"))
 
-links = [] 
-
 doc.css("div.instant").each do |link|
-	links << "https://www.myinstants.com" + link.css("a")[0]["href"]
+	@links << "https://www.myinstants.com" + link.css("a")[0]["href"]
 end 
+
+def print_links
+	@links.each do |l|
+		puts l 
+	end 
+end 
+
+print_links
 	
 	
